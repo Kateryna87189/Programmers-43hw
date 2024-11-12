5e1ccae3775cb82e193508e51d262b4f571a2d94
Kateryna Nuzhna <katranchik21@gmail.com>
2024-11-12 02:02:01 +0100

    finaly commit

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,20 @@ a )  Дан список Programmer(String name, String city, List<Task>  tasks)
 Каждый программист  имеет список задач
 Task (int Number, String description, String status, int daysInProcessing) .
 Сформировать лист из всех задач.
-
-b)  Дан список Programmer(String name, String city, List<Task>  tasks).
-Каждый программист  имеет список задач
-Task (int Number, String description, String status, int daysInProcessing) .
+b)
 Сформировать лист из всех задач, которые относятся к программистам из Берлина, не завершены
 (т.е. имеют статус, отличный от «done») и висят в обработке более 5 дней.
- */
+
+c) Сформировать Map<Boolean, Task> , где ключу true будут соответствовать задачи находящиеся в обработке
+(поле daysInProcessing)  менее заданного количества дней, false - остальные.
+
+d)Сформировать Map<Integer, Task> , где ключ сколько дней задача в обработке, значение - список задач.
+*/
 public class Main {
     public static void main(String[] args) {
         List<Programmer> programmers = List.of(
                 new Programmer("Максим ", "Берлін", List.of(
-                        new Task(1, "Виправити помилку у функції входу", "in process", 10),
+                        new Task(1, "Виправити помилку у функції входу", "in process", 2),
                         new Task(2, "Розробити новий модуль", "done", 7),
                         new Task(3, "Написати документацію", "not started", 6)
                 )),
@@ -34,11 +36,12 @@ public class Main {
                 ))
         );
         //Сформировать лист из всех задач.
-        List<Task> allTask = programmers.stream()
-                .flatMap(p -> p.getTasks().stream())
+        List<String> allTaskWithNames = programmers.stream()
+                .flatMap(p->p.getTasks().stream()
+                        .map(t->p.getName()+"-"+t.getDescription()))
                 .collect(Collectors.toList());
-        System.out.println("------------Усі задачі-------------------------");
-        allTask.forEach(t -> System.out.println(t.getDescription()));
+        System.out.println("Задачі програмістів:\n");
+        allTaskWithNames.forEach(System.out::println);
 
         //Сформировать лист из всех задач, которые относятся к программистам из Берлина, не завершены
         Map<String, List<Task>> taskByProgrammer = programmers.stream()
@@ -51,11 +54,31 @@ public class Main {
                                 .collect(Collectors.toList())
                 ));
         System.out.println("--------------------------------------------------------------------------------------------");
-        System.out.println("Задачі з Берліна що не завершені та в обробці більше 5 днів: ");
+        System.out.println("Задачі з Берліна що не завершені та в обробці більше 5 днів:\n ");
         taskByProgrammer.forEach((name, tasks) -> {
             System.out.println("прогаміст: " + name);
             tasks.forEach(t -> System.out.println(" - " + t.getDescription()));
         });
+
+        //c)
+        int daysTreshold = 5;
+        Map<Boolean, List<Task>> tasksByProcessingTime = programmers.stream()
+                .flatMap(p->p.getTasks().stream())
+                .collect(Collectors.partitioningBy(t->t.getDaysInProcessing()<daysTreshold));
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("\nЗадачі, розподілені за кількістю днів в обробці:\n ");
+        tasksByProcessingTime.forEach((isBelowTreshold, tasks)->
+                tasks.forEach(t->
+                        System.out.println((isBelowTreshold ? "<" + daysTreshold+ "day: ": ">== "+daysTreshold+"days)")+
+                t.getDescription() + "(" + t.getDaysInProcessing() + " days)")));
+
+        //d
+        Map<Integer, List<Task>> taskByDaysInProcessing= programmers.stream()
+                .flatMap(p->p.getTasks().stream())
+                .collect(Collectors.groupingBy(Task::getDaysInProcessing));
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("\nЗадачі, згруповані за кількістю днів в обробці:\n "+taskByDaysInProcessing);
+
 
 
     }
